ac94863f29eb52a83b2ea793a5c7240a7014aa18
VICT0R89 <victor4guiar89@gmail.com>
2023-08-14 20:29:02 -0300

    pre parcial

---
+++ src/parcial/AppParcial.jsx
@@ -0,0 +1,12 @@
+import {Card} from "./Card";
+
+
+export const AppParcial = () => {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
